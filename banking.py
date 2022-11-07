7c420156b0f45ba9bc2ac45ff94bd0d7b654a91e
Deni Turku <117440428+deniT10@users.noreply.github.com>
2022-11-07 19:04:00 +0100

    Update banking.py

--- banking.py
+++ banking.py
@@ -5,23 +5,33 @@ class Account():
         self.balance = balance
 
     def deposit(self):
-        deposite_amount = int(input("\nYour currently have "+str(self.balance)+ " $\nHow much money do you want to deposite?\n"))
-        self.balance = self.balance + deposite_amount
-        print(str(deposite_amount)+"$ have been deposite to your account.\nThe current amount is "+str(self.balance)+"$")
+        while(True):
+            try:
+                deposite_amount = int(input("\nYour currently have "+str(self.balance)+ " $\nHow much money do you want to deposite?\n"))
+                self.balance = self.balance + deposite_amount
+                print(str(deposite_amount)+"$ have been deposite to your account.\nThe current amount is "+str(self.balance)+"$")
+                break
+            except Exception as ex:
+                print("please input a valid number")
+                deposite_amount = 0
+        
 
     def withdraw(self):
-        withdraw_amount = int(input("\nYour currently have "+str(self.balance)+ " $\nHow much money do you want to withdraw?\n"))
-        if withdraw_amount <= self.balance:
-            self.balance = self.balance - withdraw_amount
-            print(str(withdraw_amount)+ "$ have been withdrawn.\nThe current amount is "+str(self.balance)+"$")
-        else:
-            print("You dont have enough money to perform this withdrawel")
+        while(True):
+            try:
+                withdraw_amount = int(input("\nYour currently have "+str(self.balance)+ " $\nHow much money do you want to withdraw?\n"))
+                if withdraw_amount <= self.balance:
+                    self.balance = self.balance - withdraw_amount
+                    print(str(withdraw_amount)+ "$ have been withdrawn.\nThe current amount is "+str(self.balance)+"$")
+                    break
+                else:
+                    print("You dont have enough money to perform this withdrawel")
+            except:
+                print("\nPlease input a valid number")
     
     def login(self):
-        psw = input(f"Please put in the Account Password: ")
-        while( psw != "123456"):
-            psw = input("You entered the wrong password. Please enter the right password: ")            
-        else:
+        psw = input("Put in the password: ")
+        if( psw == "123456"):
             self.start()
 
     def GetBalance(self):
@@ -29,7 +39,7 @@ class Account():
 
     def start(self):
         while(True):
-            action = input("\nPress 1 to chech the balance\nPress 2 to withdraw money\nPress 3 to deposite\nPress 4 to log out\n")
+            action = input("\nPress 1 to check the balance\nPress 2 to withdraw money\nPress 3 to deposite\nPress 4 to log out\n")
             if(action ==  "1"):
                 self.GetBalance()
             elif(action == "2"):
